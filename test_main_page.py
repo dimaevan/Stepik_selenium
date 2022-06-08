c05930c2ab240aac0a2b4d129e04735041beb505
dimaevan <dimaevan@gmail.com>
2022-06-08 19:27:02 +0300

    Add functional to redirect page

--- test_main_page.py
+++ test_main_page.py
@@ -7,13 +7,5 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    page.should_be_login_link()
-
-
-def test_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
-    page.should_be_login_form()
-    page.should_be_register_form()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
